757f9cd862740196d5c59b1b5e5e6ca7e5a4feb6
Boombayaah <132472871+Boombayaah@users.noreply.github.com>
2026-06-08 01:49:21 +0700

    Update comment

--- 2. application_aku-sehat/stakeholder_user/aplikasiWebmo/app/src/main/java/com/example/aplikasiwebmo/network/NetworkConfig.java
+++ 2. application_aku-sehat/stakeholder_user/aplikasiWebmo/app/src/main/java/com/example/aplikasiwebmo/network/NetworkConfig.java
@@ -1,6 +1,6 @@
 package com.example.aplikasiwebmo.network;
 
 public class NetworkConfig {
-//  Sesuaikan dengan IP perangkat pribadi
+//  Sesuaikan dengan IPv4 perangkat pribadi
     public static final String BASE_URL = "http://192.168.1.8/aku_sehat/php/";
 }
