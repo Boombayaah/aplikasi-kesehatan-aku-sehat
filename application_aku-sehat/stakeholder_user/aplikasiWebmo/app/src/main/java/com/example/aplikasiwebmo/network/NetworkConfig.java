ac3d2fe8214ed05e051d9e8359d0041322549e9a
Boombayaah <132472871+Boombayaah@users.noreply.github.com>
2026-06-07 19:25:06 +0700

    Tambah komentar network

--- application_aku-sehat/stakeholder_user/aplikasiWebmo/app/src/main/java/com/example/aplikasiwebmo/network/NetworkConfig.java
+++ application_aku-sehat/stakeholder_user/aplikasiWebmo/app/src/main/java/com/example/aplikasiwebmo/network/NetworkConfig.java
@@ -1,5 +1,6 @@
 package com.example.aplikasiwebmo.network;
 
 public class NetworkConfig {
-    public static final String BASE_URL = "http://192.168.88.108/aku_sehat/";
+//  Sesuaikan dengan IP perangkat pribadi
+    public static final String BASE_URL = "http://192.168.0.110/aku_sehat/php/";
 }
