848feb2d646e26e16305734fc0612d42bc6973c9
Boombayaah <132472871+Boombayaah@users.noreply.github.com>
2026-06-07 20:38:32 +0700

    Update dir

--- application_aku-sehat/stakeholder_staff/akusehat/app/src/main/java/com/example/akusehat/NetworkConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.akusehat;
-
-public class NetworkConfig {
-    public static final String BASE_URL = "http://192.168.88.108/aku_sehat/";
-}
--- application_aku-sehat/stakeholder_user/aplikasiWebmo/app/src/main/java/com/example/aplikasiwebmo/network/NetworkConfig.java
+++
@@ -1,6 +0,0 @@
-package com.example.aplikasiwebmo.network;
-
-public class NetworkConfig {
-//  Sesuaikan dengan IP perangkat pribadi
-    public static final String BASE_URL = "http://192.168.0.110/aku_sehat/php/";
-}
